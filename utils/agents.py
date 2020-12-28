bf939279171b64c9edd3bef9f3c6d6ce4776a1c8
ewanlee <ewanlee@yeah.net>
2020-12-28 20:21:43 +0800

    added independent critic and agent

--- utils/agents.py
+++ utils/agents.py
@@ -3,6 +3,7 @@ from torch.autograd import Variable
 from torch.optim import Adam
 from utils.misc import hard_update, gumbel_softmax, onehot_from_logits
 from utils.policies import DiscretePolicy
+from utils.networks import MLPNetwork
 
 class AttentionAgent(object):
     """
@@ -53,3 +54,67 @@ class AttentionAgent(object):
         self.target_policy.load_state_dict(params['target_policy'])
         self.old_policy.load_state_dict(params['old_policy'])
         self.policy_optimizer.load_state_dict(params['policy_optimizer'])
+
+class SACAgent(object):
+    """
+    General class for Attention agents (policy, target policy)
+    """
+    def __init__(self, num_in_pol, num_out_pol, num_in_critic, hidden_dim=64,
+                 lr=0.01, onehot_dim=0):
+        """
+        Inputs:
+            num_in_pol (int): number of dimensions for policy input
+            num_out_pol (int): number of dimensions for policy output
+        """
+        self.policy = DiscretePolicy(num_in_pol, num_out_pol,
+                                     hidden_dim=hidden_dim,
+                                     onehot_dim=onehot_dim)
+        self.critic = MLPNetwork(num_in_pol, num_out_pol,
+                                 hidden_dim=hidden_dim,
+                                 constrain_out=False)
+        self.target_policy = DiscretePolicy(num_in_pol,
+                                            num_out_pol,
+                                            hidden_dim=hidden_dim,
+                                            onehot_dim=onehot_dim)
+        self.old_policy = DiscretePolicy(num_in_pol,
+                                            num_out_pol,
+                                            hidden_dim=hidden_dim,
+                                            onehot_dim=onehot_dim)
+        self.target_critic = MLPNetwork(num_in_pol, num_out_pol,
+                                 hidden_dim=hidden_dim,
+                                 constrain_out=False)
+        
+        hard_update(self.target_policy, self.policy)
+        hard_update(self.old_policy, self.policy)
+        hard_update(self.target_critic, self.critic)
+        self.policy_optimizer = Adam(self.policy.parameters(), lr=lr)
+        self.critic_optimizer = Adam(self.critic.parameters(), lr=lr)
+
+    def step(self, obs, explore=False):
+        """
+        Take a step forward in environment for a minibatch of observations
+        Inputs:
+            obs (PyTorch Variable): Observations for this agent
+            explore (boolean): Whether or not to sample
+        Outputs:
+            action (PyTorch Variable): Actions for this agent
+        """
+        return self.policy(obs, sample=explore)
+
+    def get_params(self):
+        return {'policy': self.policy.state_dict(),
+                'critic': self.critic.state_dict(),
+                'target_policy': self.target_policy.state_dict(),
+                'old_policy': self.old_policy.state_dict(),
+                'target_critic': self.target_critic.state_dict(),
+                'policy_optimizer': self.policy_optimizer.state_dict(),
+                'critic_optimizer': self.critic_optimizer.state_dict()}
+
+    def load_params(self, params):
+        self.policy.load_state_dict(params['policy'])
+        self.critic.load_state_dict(params['critic'])
+        self.target_policy.load_state_dict(params['target_policy'])
+        self.old_policy.load_state_dict(params['old_policy'])
+        self.target_critic.load_state_dict(params['target_critic'])
+        self.policy_optimizer.load_state_dict(params['policy_optimizer'])
+        self.critic_optimizer.load_state_dict(params['critic_optimizer'])
